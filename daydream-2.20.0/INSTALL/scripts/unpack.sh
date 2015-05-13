18eded12c91d3ce15d9081539a21841e39ba323c
Ryan Fantus <ryanfantus@gmail.com>
2015-05-13 10:39:13 -0700

    Update unpack.sh

--- daydream-2.20.0/INSTALL/scripts/unpack.sh
+++ daydream-2.20.0/INSTALL/scripts/unpack.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+#
+# Change FIDODIR=/home/bbs/fido to your actual echomail dir. Beneath that should be /in, /in.sec, /in.loc, /out
+#
+FIDODIR=/home/bbs/fido
 PKTS=`find $INBOUND -iname *.mo? -or -iname *.tu? -or -iname *.we? -or -iname *.th? -or -iname *.fr? -or -iname *.sa? -or -iname *.su? | xargs`
 
 for i in $PKTS 
@@ -8,7 +11,7 @@ do
   if [ -n "`echo $x|grep -i zip`" ];
   then
     unzip $i -d `dirname $i`
-    [ -x /usr/bin/fromdos ] && fromdos in*/*.pkt || echo "Install package tofrodos for full functionality"
+    [ -x /usr/bin/fromdos ] && fromdos $FIDODIR/in*/*.pkt || echo "Install package tofrodos for full functionality"
     if [ $? -eq 0 ]; then
       rm -f $i
     fi
